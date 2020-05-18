c4a990b55b43b3ec4449eadc79bf6cf4145bc6c8
Jhonathan Alfonso <salajhon69@gmail.com>
2020-05-18 04:24:04 -0500

    Promises JS

--- callbacks.js
+++ callbacks.js
@@ -2,37 +2,40 @@ const API_URL = 'https://swapi.dev/api/';
 const PEOPLE_URL ='people/:id';
 const opts = { crossDomain: true };
 
-function getCharacter(id, callback) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+function getCharacter(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+        $
+            .get(url, opts, function(data) {
+                resolve(data)
+            })
+            .fail(() => reject(id))
+            })
+    }
 
-    $
-    .get(url, opts, callback);
-    .fail(() => {
-        console.log(`Sucedió un erro. No se pudo obtener el personaje ${id}`);
-    });
+
+function onError(id){
+    console.log(`Sucedió un error al obtener el personaje ${id}`);
 }
 
-getCharacter(1, function(persona) {
-    console.log(`Hola, yo soy ${persona.name}`);
-
-    getCharacter(2, function(persona) {
-        console.log(`Hola, yo soy ${persona.name}`);
-
-        getCharacter(3, function(persona) {
-            console.log(`Hola, yo soy ${persona.name}`);
-
-            getCharacter(4, function(persona) {
-                console.log(`Hola, yo soy ${persona.name}`);
-
-                getCharacter(5, function(persona) {
-                    console.log(`Hola, yo soy ${persona.name}`);
-
-                    getCharacter(6, function (persona){
-                        console.log(`Hola, yo soy ${persona.name}`);
-                    })
-                });
-            });
-        });
-    });
-});
-
+getCharacter(1)
+    .then((persona) => {
+        console.log(`El personaje 1 es ${persona.name}`);
+        return getCharacter(2);
+    })
+    .then(persona => {
+        console.log(`El personaje 2 es ${persona.name}`);
+        return getCharacter(3);
+    })
+    .then(persona => {
+        console.log(`El personaje 3 es ${persona.name}`);
+        return getCharacter(4);
+    })
+    .then(persona => {
+        console.log(`El personaje 4 es ${persona.name}`);
+        return getCharacter(5);
+    })
+    .then(persona => {
+        console.log(`El personaje 6 es ${persona.name}`);
+    })
+    .catch(onError);
