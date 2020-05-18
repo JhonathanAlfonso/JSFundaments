be9b97a84fb3efc9a032b4026f1e835da3b5f86f
Jhonathan Alfonso <salajhon69@gmail.com>
2020-05-18 04:48:56 -0500

    Promises Advanced

--- callbacks.js
+++ callbacks.js
@@ -18,24 +18,37 @@ function onError(id){
     console.log(`Sucedió un error al obtener el personaje ${id}`);
 }
 
-getCharacter(1)
-    .then((persona) => {
-        console.log(`El personaje 1 es ${persona.name}`);
-        return getCharacter(2);
-    })
-    .then(persona => {
-        console.log(`El personaje 2 es ${persona.name}`);
-        return getCharacter(3);
-    })
-    .then(persona => {
-        console.log(`El personaje 3 es ${persona.name}`);
-        return getCharacter(4);
-    })
-    .then(persona => {
-        console.log(`El personaje 4 es ${persona.name}`);
-        return getCharacter(5);
-    })
-    .then(persona => {
-        console.log(`El personaje 6 es ${persona.name}`);
-    })
-    .catch(onError);
+async function getCharacteres() {
+    var ids = [1, 2, 3, 4, 5, 6, 7]
+    var promises = ids.map(id => getCharacter(id))
+    try {
+        var personajes = await Promise.all(promises)
+        console.log(personajes)
+    } catch(id) {
+        onError(id)
+    }
+}
+
+getCharacteres()
+
+// getCharacter(1)
+//     .then((persona) => {
+//         console.log(`El personaje 1 es ${persona.name}`);
+//         return getCharacter(2);
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 2 es ${persona.name}`);
+//         return getCharacter(3);
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 3 es ${persona.name}`);
+//         return getCharacter(4);
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 4 es ${persona.name}`);
+//         return getCharacter(5);
+//     })
+//     .then(persona => {
+//         console.log(`El personaje 6 es ${persona.name}`);
+//     })
+//     .catch(onError);
